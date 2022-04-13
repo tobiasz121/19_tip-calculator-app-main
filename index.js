a7e006b635500001fa0d0db4eb52e2d7bac2254c
tobiasz121 <78846581+tobiasz121@users.noreply.github.com>
2022-04-13 15:24:33 +0200

    commit

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
-const buttons = document.querySelectorAll('button')
-const custm = document.querySelector('input.tip-btn')
-//Go through each button, if button was active, after click make it inactive
-//Styling is applied throuh css and attribute selector. Each button with ariaselected = 'true' has some 
-//styling applied
+// const buttons = document.querySelectorAll('button')
+// const custm = document.querySelector('input.tip-btn')
+// const pTot = document.querySelector('#total-person')
+// const pTip = document.querySelector('#tip-person')
+// //Go through each button, if button was active, after click make it inactive
+// //Styling is applied throuh css and attribute selector. Each button with ariaselected = 'true' has some 
+// //styling applied
 
-buttons.forEach(button => {
-    button.addEventListener('click', event => {
-        event.preventDefault        
-        if (button.ariaSelected === 'true'){
-            button.ariaSelected = 'false'
-            
-        }
-        else {
-            //If clicked button was inactive, first make sure that all other button are inactive,
-            //and then make current target active.
-            buttons.forEach(button => {
-                button.ariaSelected = 'false'
-            })
-            button.ariaSelected = 'true'
-            custm.value = ''
+// buttons.forEach(button => {
+//     button.addEventListener('click', event => {
+//         event.preventDefault        
+//         if (button.ariaSelected === 'true'){
+//             button.ariaSelected = 'false'
+//             pTot.textContent = `$0.00`
+//             pTip.textContent = `$0.00`
+//         }
+//         else {
+//             //If clicked button was inactive, first make sure that all other button are inactive,
+//             //and then make current target active.
+//             buttons.forEach(button => {
+//                 button.ariaSelected = 'false'
+//             })
+//             button.ariaSelected = 'true'
+//             custm.value = ''
 
-        }
-    })
-})
+//         }
+//     })
+// })
--- total.js
+++ total.js
@@ -4,6 +4,7 @@ const personTip = document.querySelector('#tip-person')
 const custombtn = document.querySelector('input.tip-btn')
 const reset = document.querySelector('.summary-reset')
 
+
 //Totals function used to calculate correct tip and total for person.
 //It takes current target parameter, and type of the target.
 const totals = (curBtn, type) => {
@@ -20,18 +21,23 @@ const totals = (curBtn, type) => {
     }  
     
     //Bill and people fields values, converted to numbers
-    const bill = Number( document.querySelector('#bill').value, 10)
+    const bill = Number( document.querySelector('#bill').value, 10)    
     const people = Number(document.querySelector('#people').value, 10)
+    const people2 = Number.parseInt(document.querySelector('#people').value, 10)  
+    console.log(people2)
 
     //Change placeholder values for the input fields, if they're empty. 
     //If they're empty change button state to inactive as well
-    //if there are value, calculate the amounts and pass them to the correct html targets.
-    if (bill === 0){            
-        document.querySelector('#bill').placeholder = 'Insert Bill'
+    //if there are value, calculate the amounts and pass them to the correct html targets.    
+
+    if (bill === 0 || Number.isNaN(bill)){   
+        document.querySelector('#bill').value= ''         
+        document.querySelector('#bill').placeholder = 'Insert Bill (Number)'
         curBtn.ariaSelected = 'false'
     }
-    if (people ===0 ){
-        document.querySelector('#people').placeholder = 'Insert No of People'
+    if (people ===0 || Number.isNaN(people) || people %1 !=0){        
+        document.querySelector('#people').value= ''
+        document.querySelector('#people').placeholder = 'Insert No of People(Int) '
         curBtn.ariaSelected = 'false'
     }
     else  {
@@ -41,12 +47,30 @@ const totals = (curBtn, type) => {
     
 }
 
-//Monitor each tip button, after click action on any given button, invoke totals function and pass current target as parameter.
+// Go through each button, if button was active, after click make it inactive
+// Styling is applied throuh css and attribute selector. Each button with ariaselected = 'true' has some 
+// styling applied
+//After click and aria check, invoke totals function and pass current target as parameter.
 //2nd parameter distingushes button from input, as data is taken differently from them (dataset vs value)
 buttons1.forEach(button => {
     button.addEventListener('click', event => {
         const curBtn = event.currentTarget
-        totals(curBtn, 'button')
+        if (button.ariaSelected === 'true'){
+            button.ariaSelected = 'false'
+            personTotal.textContent = `$0.00`
+            personTip.textContent = `$0.00`
+        }
+        else {
+            //If clicked button was inactive, first make sure that all the other buttons are inactive,
+            //and then make current target active.
+            buttons1.forEach(button => {
+                button.ariaSelected = 'false'
+            })
+            button.ariaSelected = 'true'
+            custombtn.value = ''
+            totals(curBtn, 'button')
+        }        
+        
     })
 })        
        
